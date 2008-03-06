b9f557fe065d8af411186a498d987d001effb196
martinhowse <martinhowse>
2008-03-06 18:50:43 +0000

    Ported to sole dependency on MLT framework (Miracle server).

--- gneve.el
+++ gneve.el
@@ -43,25 +43,16 @@
 
 ;;; Dependency:
 
-;; gnevel.el requires mplayer 1.0 and avidemux 2.4, lastest svn version recommended
-;; mplayer:  patch time postition precision to 6 digits
-;; -- checkout: svn checkout svn://svn.mplayerhq.hu/mplayer/trunk mplayer
-;; -- patch:
-;;--- command.c	(revision 23382)
-;;+++ command.c	(working copy)
-;;@@ -2325,7 +2325,7 @@
-;; 		    pos =
-;; 			playing_audio_pts(sh_audio, mpctx->d_audio,
-;; 					  mpctx->audio_out);
-;;-		mp_msg(MSGT_GLOBAL, MSGL_INFO, "ANS_TIME_POSITION=%.1f\n", pos);
-;;+		mp_msg(MSGT_GLOBAL, MSGL_INFO, "ANS_TIME_POSITION=%.6f\n", pos);
-;; 	    }
-;; 	    break;
-;; -- install: ./configure ; make ; su ; make install
+;; gneve.el now only requires MLT framework (subversion) 
+;; Miracle server running on port 5250
 
-;; avidemux:  use svn version
-;; -- checkout: svn checkout svn://svn.berlios.de/avidemux/branches/avidemux_2.4_branch/
-;; -- install: make -f Makefile.dist ; ./configure ; make ; su ; make install
+;;; TODO:
+
+; 0] render options
+; 1] Add support for transitions
+; 2] Subtitle editor built on same foundations
+; 3] Fix SDL/X11 and size of clip
+; 4] Support for org-mode linking to re-write and place here
 
 ;;; Code:
 
@@ -89,11 +80,9 @@
 (defvar end nil "end of timecode mark")
 (defvar videoname nil "rendered videofile name")
 (defvar timecode-string nil "timecode string")
-(defvar tc-hour nil "timecode hour part")
-(defvar tc-min nil "timecode minute part")
-(defvar tc-sec nil "timecode second part")
-(defvar tc-msec nil "timecode mili second part")
+(defvar gn-connection nil "connection to miracle server")
 
+(setq gn-connection nil)
 
 ;; Keyboard shortcuts definition
 
@@ -104,12 +93,15 @@
 ;; Video operation
   (define-key gneve-mode-map "V" 'open-film)
   (define-key gneve-mode-map "L" 'pause)
+  (define-key gneve-mode-map "M" 'play)
   (define-key gneve-mode-map "J" 'prev-frame)
   (define-key gneve-mode-map "K" 'next-frame)
   (define-key gneve-mode-map "Q" 'one-sec-back)
   (define-key gneve-mode-map "W" 'one-sec-forward)
   (define-key gneve-mode-map "A" 'five-sec-back)
   (define-key gneve-mode-map "S" 'five-sec-forward)
+
+
 
 ;; Mark operation
   (define-key gneve-mode-map "E" 'mark-start)
@@ -120,34 +112,30 @@
   (define-key gneve-mode-map "C" 'goto-point)
   (define-key gneve-mode-map "G" 'goto-timecode)
 
-;; Render operation
-  (define-key gneve-mode-map "U" 'region-render))
+  ;; Render operation
+  (define-key gneve-mode-map "U" 'region-render)
   (define-key gneve-mode-map "I" 'buffer-render)
   (define-key gneve-mode-map "O" 'save-rendered)
   (define-key gneve-mode-map "P" 'play-rendered)
-  (define-key gneve-mode-map "D" 'take-screenshot)
-
+  (define-key gneve-mode-map "[" 'preview))
 
 (defun gneve-mode()
   "EDL and mplayer based GNU Emacs video editing mode
 
 Video commands:
   V - Visit video file and start playing
-  L - Pause/Play
+  L - Pause
+  M - Play
   J - 1 frame back and pause
   K - 1 frame forward and pause
-  Q - 1 second back and pause 
-  W - 1 second forward and pause
-  A - 5 seconds back and pause
-  S - 5 seconds forward and pause
   C - Goto timecode of current point
   G - Goto timecode of user input
 
-  Layout summary:
+  Layout summary.
 
   Q W 
    A S     G H J K L
-          V   
+          V     M
 
 Editing commands:
   E - Mark start of a section
@@ -166,9 +154,10 @@ Render commands:
   I - Render whole buffer
   O - Save rendered video
   P - Play rendered video
+  [ - preview video (to-be-rendered)
 
   Layout summary:  
-              U I O P"
+              U I O P ["
   (interactive)
   (kill-all-local-variables)
   (defvar vslot-p nil "vslot predicate")
@@ -205,24 +194,34 @@ Render commands:
     (insert (format "\n\"%s\"" (expand-file-name filename)))
     (forward-char 2))
   (setq vslot-n (vslot-pos (expand-file-name filename) (reverse vslots)))
-  (start-process "my-process" "boo" "mplayer" "-slave" "-vo" "x11" "-xy" "320" "-osdlevel" "1" "-quiet" (expand-file-name filename))
+;; if buffer doesn't already exist
+   (when (not gn-connection)
+ (setq gn-connection (make-comint "boo" (cons "127.0.0.1" 5250)))
+ (process-send-string gn-connection "UADD sdl\n")
+ (process-send-string gn-connection "UADD avformat\n"))
+   (process-send-string gn-connection (format "LOAD U%d %s\n" vslot-n (expand-file-name filename)))
+ (process-send-string gn-connection (format "PLAY U%d\n" vslot-n))
   (print (expand-file-name filename)))
-
-(defun take-screenshot ()
-  (interactive)
-  (process-send-string "my-process" "screenshot 0\n"))
 
 (defun next-frame ()
   (interactive)
-  (process-send-string "my-process" "frame_step\n"))
+   (process-send-string gn-connection (format "STEP U%d 1\n" vslot-n)))
 
 (defun prev-frame ()
   (interactive)
-  (process-send-string "my-process" "seek -0.08\npause\n"))
+   (process-send-string gn-connection (format "STEP U%d -1\n" vslot-n)))
 
 (defun pause ()
   (interactive)
-  (process-send-string "my-process" "pause\n"))
+   (process-send-string gn-connection (format "PAUSE U%d\n" vslot-n)))
+
+(defun play ()
+  (interactive)
+   (process-send-string gn-connection (format "PLAY U%d\n" vslot-n)))
+
+(defun query-film ()
+  (interactive)
+   (process-send-string gn-connection (format "USTA U%d\n" vslot-n)))
 
 (defun write-marks ()
   "write video slot, lastin, lastout time code into EDL buffer"
@@ -232,19 +231,19 @@ Render commands:
 
 (defun one-sec-back ()
   (interactive)
-  (process-send-string "my-process" "seek -1.0\npause\n"))
+   (process-send-string gn-connection (format "STEP U%d -25\n" vslot-n)))
 
 (defun one-sec-forward ()
   (interactive)
-  (process-send-string "my-process" "seek 1.0\npause\n"))
+   (process-send-string gn-connection (format "STEP U%d 25\n" vslot-n)))
 
 (defun five-sec-back ()
   (interactive)
-  (process-send-string "my-process" "seek -5.0\npause\n"))
+   (process-send-string gn-connection (format "STEP U%d -125\n" vslot-n)))
 
 (defun five-sec-forward ()
   (interactive)
-  (process-send-string "my-process" "seek 5.0\npause\n"))
+   (process-send-string gn-connection (format "STEP U%d 125\n" vslot-n)))
 
 (defun mark-start ()
   (interactive)
@@ -261,29 +260,28 @@ Render commands:
   (interactive)
   (progn
     ;; goto end of buffer search back to equals and copy to last-in
-    (set-buffer "boo")
-    (process-send-string "my-process" "pausing get_time_pos\n")
+    (query-film)
+    (set-buffer "*boo*")
     (sleep-for 0.1)
     (goto-char (point-max))
-    (backward-char)
-    (backward-char)
-    (setq end (point))
-    (search-backward "=")
-    (forward-char)
-    (setq start (point))
-    (copy-region-as-kill start end)
-    (car kill-ring))) ;; stend
+    (previous-line)
+    (re-search-forward "\"")
+    (re-search-forward "\"")
+    ;; search for last number and return this
+    (re-search-forward number-regexp)
+    (goto-char (match-end 0)))
+  (buffer-substring (match-beginning 0) (point)))
 
 (defun goto-point ()
   (interactive)  
   (setq timecode-string (read (current-buffer)))
-  (process-send-string "my-process" (format "seek %s 2\npause\n" timecode-string)))
+   (process-send-string gn-connection (format "GOTO U%d %s\n" vslot-n timecode-string)))
 
 (defun tc-human ()
   "calculate human readable timecode (hh:mm:ss,ms)"
-  (setq tc-hour (/ (floor (string-to-number timecode-string)) 3600))
-  (setq tc-min (/ (- (floor (string-to-number timecode-string)) (* 3600 tc-hour))  60))
-  (setq tc-sec (- (string-to-number timecode-string) (* 60 tc-min) (* 3600 tc-hour)))
+  (setq tc-hour (/ (floor (string-to-number timecode-string)) 9000))
+  (setq tc-min (/ (- (floor (string-to-number timecode-string)) (* 9000 tc-hour))  1500))
+  (setq tc-sec (- (string-to-number timecode-string) (* 1500 tc-min) (* 9000 tc-hour)))
   (setq tc-msec (truncate (* 1000 (- tc-sec (floor tc-sec))))))
 
 
@@ -296,18 +294,18 @@ Render commands:
   (setq timecode-newpos  (read-string "Goto timecode (min:sec) " (format "%d:%.2f" tc-min tc-sec ) nil nil))
   (setq tc-min (car (split-string timecode-newpos ":")))
   (setq tc-sec (cadr (split-string timecode-newpos ":")))
-  (setq timecode-string (number-to-string (+ (* 60 (string-to-number tc-min)) (string-to-number tc-sec))))
-  (process-send-string "my-process" (format "seek %s 2\npause\n" timecode-string)))
+  (setq timecode-string (number-to-string (+ (* 1500 (string-to-number tc-min)) (* 25 (string-to-number tc-sec)))))
+  (process-send-string gn-connection (format "GOTO U%d %s\n" vslot-n timecode-string)))
 
 (defun goto-start ()
   "goto mark start"
   (interactive)
-  (process-send-string "my-process" (format "seek %s 2\npause\n" lastin)))
+  (process-send-string gn-connection (format "GOTO U%d %s\n" vslot-n lastin)))
 
 (defun goto-end ()
   "goto mark end"
   (interactive)
-  (process-send-string "my-process" (format "seek %s 2\npause\n" lastout)))
+  (process-send-string gn-connection (format "GOTO U%d %s\n" vslot-n lastout)))
 
 (defun buffer-render ()
   "render whole buffer"
@@ -316,8 +314,8 @@ Render commands:
     (save-restriction
       (goto-char (point-min))
       (narrow-to-region (+ (search-forward  "))") 2) (point-max)) 
-      (render)))
-  (pop-to-buffer "avidemux"))
+      (render)
+      (post-render))))
 
 (defun region-render ()
   "render only active region"
@@ -325,8 +323,8 @@ Render commands:
   (save-excursion
     (save-restriction
       (narrow-to-region (region-beginning) (region-end))
-      (render)))
-  (pop-to-buffer "avidemux"))
+      (render)
+      (post-render))))
 
 (defun save-rendered ()
   "save rendered video file"
@@ -336,25 +334,30 @@ Render commands:
   (or (y-or-n-p (format "Video `%s' already exists; overwrite? " videoname))
 		 (error "Canceled")))
   (message videoname)
-  (shell-command (concat "if [ -f \"/tmp/test.srt\" ]; then cp -f /tmp/test.srt " (expand-file-name videoname) ".srt; fi"))
-  (shell-command (concat "time cp -f /tmp/test.avi " (expand-file-name videoname))))
+  (shell-command (concat "time cp -f /tmp/test.dv " (expand-file-name videoname))))
+
+(defun preview ()
+       (interactive)
+  ;; write XML
+  (render)
+  ;; play with inigo editor
+  (start-process "non-process3" nil "inigo" "/tmp/basic.westley"))
 
 (defun play-rendered ()
   "play rendered video file"
   (interactive)
-  (start-process "my-process3" nil "mplayer" "-vo" "x11" "-sub" "/tmp/test.srt" "-quiet" "/tmp/test.avi"))
+  (start-process "non-process3" nil "mplayer" "-vo" "x11" "-sub" "/tmp/test.srt" "-quiet" "/tmp/test.dv"))
 
 (defun render()
-  "render edl to avidemux js script"
+  "render edl to westley/openMLT XML"
   (interactive)
   (defvar startframe nil "start frame")
   (defvar endframe nil "end frame")
   (defvar subcounter 1 "subtitle counter")
   (defvar subtitle nil "subtitle string")
-  (defvar lengthrendered 0 "length of rendered film")
   (setq subcounter 1)
-  (setq lengthrendered 0)
-  ;; count segments and write header with this info and source file
+
+;; counts number of edits
   (let ((old-point (point))
         (counter 0)
 	(x 0))
@@ -364,92 +367,65 @@ Render commands:
       (setq counter (1+ counter)))
     (goto-char old-point)
     (message "%d" counter)
-    (switch-to-buffer (find-file-noselect "/tmp/test.srt"))
-    (erase-buffer)
-    (switch-to-buffer (find-file-noselect "/tmp/gnevetemp.js")) ;; should be blank
-    (erase-buffer)
-    (insert "//AD\n//created by gneve.el\nvar app = new Avidemux();\n")
-    (insert (format "app.load(\"%s\");\n" (car vslots)))
-    (dolist (i (cdr vslots))
-      (insert (format "app.append(\"%s\");\n" i)))
-    (insert "app.clearSegments();\n")
+
+    ;; header
+  (switch-to-buffer (find-file-noselect "/tmp/basic.westley")) ;; should be blank
+  (erase-buffer)
+  (insert "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<westley root=\"/tmp\" title=\"TMP\">\n")
+  ;; producers
+  (let ((xxx 0))
+    (dolist (i vslots)
+      (insert (format "  <producer id=\"producer%d\">\n" xxx))
+      (insert (format "    <property name=\"resource\">%s</property>\n" i))
+      (insert "  </producer>\n")
+      (setq xxx (+ xxx 1))))
+
+  (insert "  <playlist id=\"playlist0\">\n")
+
+;; EDL
     (switch-to-buffer gneve-buffer)
     (goto-char (point-min))
     (while (< x counter)
       (vslot-matcher)
       (setq startframe (timecode-matcher))
       (forward-char 1)
-      (switch-to-buffer  "gnevetemp.js")
-      (insert (format "app.addSegment(%s,%d," vslot startframe))
       (setq x (1+ x))
-      (switch-to-buffer gneve-buffer)
       (setq endframe (timecode-matcher))
-      ;; if there is subtitle string insert into srt buffer
-      (unless (eolp)
-	(looking-at ".+")
-	(setq subtitle (match-string 0))
-	(switch-to-buffer "test.srt")
-	(insert (format "%d\n" subcounter))
-	(setq timecode-string (number-to-string (/ lengthrendered 25)))
-        (tc-human)
-	(insert (format "%s:%s:%d,%s --> " tc-hour tc-min tc-sec tc-msec))
-	(setq timecode-string (number-to-string (/ (+ lengthrendered (- endframe startframe)) 25)))
-        (tc-human)
-	(insert (format "%s:%s:%d,%s\n" tc-hour tc-min tc-sec tc-msec))
-	(insert (format "%s\n\n" subtitle))
-	(setq subcounter (1+ subcounter)))
-      (switch-to-buffer  "gnevetemp.js")
-      (insert (format "%d);\n" (- endframe startframe))) ;; length
-      (setq lengthrendered (+ (- endframe startframe) lengthrendered))
+      (switch-to-buffer  "basic.westley")
+      (insert (format  "    <entry producer=\"producer%s\" in=\"%s\" out=\"%s\"/>\n" vslot startframe endframe)) ;; vslot number???
       (switch-to-buffer gneve-buffer)
       (forward-line 1)
       (beginning-of-line))
     ;; write footer
-    (progn
-      (switch-to-buffer "gnevetemp.js")
-      (goto-char (point-max))
-      (insert (format "app.markerA=0;\napp.markerB=%d;\n" (- lengthrendered 1)))
-      (insert (format "app.video.setPostProc(3,3,0);\napp.video.setFps1000(25000);app.video.codec(\"Copy\",\"CQ=4\",\"0\");app.audio.reset();app.audio.codec(\"copy\",128,0,\"\");app.audio.normalizeMode=0;app.audio.normalizeValue=0;app.audio.delay=0;app.audio.mixer(\"NONE\");app.setContainer(\"AVI\");setSuccess(app.save(\"/tmp/test.avi\"));"))
-      ;; save and close temp file
-      (save-buffer "/tmp/gnevetemp.js")
-      (switch-to-buffer "test.srt")
-      (save-buffer "/tmp/test.srt")
-      ;; deal with avidemux
-      (start-process "my-process2" "avidemux" "avidemux2_cli" "--run" "/tmp/gnevetemp.js" "--video-process" "--save" "/tmp/test.avi" "--quit")
-      (kill-buffer "gnevetemp.js")
-      (kill-buffer "test.srt")
-      (switch-to-buffer gneve-buffer)
-      (goto-char old-point))))
 
-(defun timecode-matcher ()
+      (switch-to-buffer "basic.westley")
+      (goto-char (point-max))
+      (insert "  </playlist>\n</westley>\n")
+
+      ;; save and close temp file
+      (save-buffer "/tmp/basic.westley")
+      (switch-to-buffer gneve-buffer)
+      (goto-char old-point)))
+
+(defun post-render ()
+  ;; render the xml to .... dv or ....?
+  ;; using inigo
+
+  (start-process "non-process3" nil "inigo" "/tmp/basic.westley" "-consumer" "avformat:/tmp/test.dv"))
+
+(defun timecode-matcher () ;; fix to search forwards
   (let (timecode-string)
-    (if (looking-at number-regexp)
-	(goto-char (match-end 0)))
+    (re-search-forward number-regexp)
+	(goto-char (match-end 0))
     (setq timecode-string (buffer-substring (match-beginning 0) (point)))
-    (micros-to-frame (string-to-number timecode-string))))
+    (string-to-number timecode-string)))
 
 (defun vslot-matcher ()
-  (if (looking-at vslot-regexp)
-      (goto-char (match-end 0)) )
+  (re-search-forward vslot-regexp)
+      (goto-char (match-end 0))
   (setq vslot (buffer-substring (match-beginning 1) (- (point) 1))))
-
-(defun micros-to-frame (number)
-;; 0.04 is one frame - divide by 0.04
-  (/ number 0.04))
-
-(defun time-toframe (number)
-;; min.sec conver to frame
-  (/ number 0.04))
 
 ;;; List functions
 
-(defalias 'first 'car)
-(defalias 'second 'cadr)
-(defalias 'rest 'cdr)
-(defalias 'endp 'null)
-
-
 ;;;###autoload(add-to-list 'auto-mode-alist '("\\.edl\\'" . gneve-mode))
 (provide 'gneve-mode)
-
-;;  
